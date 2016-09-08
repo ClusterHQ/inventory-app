6d6381e7321c36b120bfc182e38e0fc5b5980477
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-09-08 11:25:20 -0400

    add util script to do some dpcli actions as its easier and clearner to use in pipeline

---
+++ utils/use_snap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script will take a Flocker Hub endpoint, volumeset
+# and snapshot as arguments and create a volume from the
+# snapshot and change the `- rethink-data:` volume to
+# use that snapshot.
+
+VS=$1
+SNAP=$2
+EP=$3
+
+# should always check for init, but not fail if init already done.
+/opt/clusterhq/bin/dpcli init || true
+/opt/clusterhq/bin/dpcli set --vhub $EP
+/opt/clusterhq/bin/dpcli sync volumeset $VS
+/opt/clusterhq/bin/dpcli pull snapshot $SNAP
+/opt/clusterhq/bin/dpcli show snapshot -d $VS
+PATH=$(sudo /opt/clusterhq/bin/dpcli create volume -s "${SNAP}" 2>&1 | grep -E -o  '\/chq\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
+sed 's@\- rethink-data:@\- '"${PATH}"':@' inventory-app/docker-compose.yml
