2e44b708366845a919736f7209411d45852d1f68
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-09-15 11:12:24 -0700

    revert change

--- frontend/sc_modules/http-handler.js
+++ frontend/sc_modules/http-handler.js
@@ -34,7 +34,6 @@ module.exports.attach = function (expressApp) {
     		if (err) throw err;
     		cursor.toArray(function(err, result) {
         		if (err) throw err;
-        		//fhf
         		console.log("Sending back Dealership results");
         		res.send(JSON.stringify(result, null, 2));
     			});
