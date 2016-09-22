be28dcc114638c4914f4e64fe325b18e71c220e1
Trevor Sullivan <trevor.sullivan@clusterhq.com>
2016-09-22 09:11:08 -0600

    Removed logging from main Compose file Tested ./startapp.ps1 PowerShell script

--- startapp.ps1
+++ startapp.ps1
@@ -3,17 +3,23 @@ This PowerShell script is an entry point to the Inventory App.
 It runs Docker Compose up, regardless of the current working directory (pwd).
 #>
 
+$VerbosePreference = 'Continue'
+
+$StdOutPath = '{0}\{1}.log' -f $env:TEMP, ([System.Guid]::NewGuid().Guid)
+$StdErrPath = '{0}\{1}.log' -f $env:TEMP, ([System.Guid]::NewGuid().Guid)
+
 $Compose = @{
     FilePath = 'docker-compose'
     ArgumentList = 'up -d --force-recreate --build'
     WorkingDirectory = $PSScriptRoot
     Wait = $true
-    RedirectStandardOutput = ($StdOutPath = '{0}\{1}.log' -f $env:TEMP, ([System.Guid]::NewGuid().Guid)) 
+    RedirectStandardOutput = $StdOutPath
+    RedirectStandardError = $StdErrPath
 }
 Start-Process @Compose
 
-Get-Content -Path $Compose.RedirectStandardOutput
+Get-Content -Path $StdOutPath, $StdErrPath
 
-Write-Host -Object 'Finished starting application'
+Write-Verbose -Message 'Finished starting application'
 
 docker ps
