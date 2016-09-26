0273a0536d206c558612557d9676685412223a4b
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-09-26 10:33:53 -0400

    working now, connection was closing outside of a callback, therfore look like connection close before query was finished

--- frontend/controllers/dealership.js
+++ frontend/controllers/dealership.js
@@ -7,8 +7,10 @@ module.exports.addDealership = function (req, res) {
 	console.log('Dealership name is ' + req.body.name);
 	dbconn = db.connect();
 	dbconn.then(function (conn) {
-		rdb.table('Dealership').insert(req.body).run(conn);
-		conn.close();
+		rdb.table('Dealership').insert(req.body).run(conn, function(err, results){
+			console.log(results);
+			conn.close();
+		});
 	});
 	res.send({message: 'hello'})
 }
