76e8d73cf629426fe4d0cc56699ece581645864f
Trevor Sullivan <trevor@trevorsullivan.net>
2016-10-04 11:09:15 -0700

    Removing CLI client from this branch

--- client/launch.ps1
+++
@@ -1,5 +0,0 @@
-### Create Python Virtual Environment and change context into it
-$VirtualEnvPath = "$env:USERPROFILE\pyclient"
-virtualenv "$VirtualEnvPath"
-& $VirtualEnvPath\scripts\activate.ps1 
-
