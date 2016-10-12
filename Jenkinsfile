3eae780db6757966445ef23e32970b43e3758c00
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-10-12 10:40:44 -0400

    add staging to parallel testing branch

--- Jenkinsfile
+++ Jenkinsfile
@@ -95,3 +95,59 @@ def run_group(test, volsnap, volset) {
    sh "sudo inventory-app/ci-utils/runtest.sh ${run_test} ${vs} ${ep} ${snap} ${env.BRANCH_NAME} ${env.BUILD_NUMBER} ${env.BUILD_ID} ${env.BUILD_URL} '${env.NODE_NAME}'"
 
 }
+
+node ('v8s-dpcli-prov-staging') {
+   stage 'Staging: Make sure cloud-init done'
+   // Cloud-init runs on new jenkins slaves to install dpcli and docker, make sure its done.
+   sh "timeout 1080 /bin/bash -c   'until stat /var/lib/cloud/instance/boot-finished 2>/dev/null; do echo waiting for boot to finish ...; sleep 10; done'"
+
+   stage 'Staging: See cloud-init log'
+   // In case of failure, its nice to have this log
+   sh 'cat /var/log/cloud-init.log'
+
+   stage 'Staging: Announce'
+   echo "Starting staging on: '${env.NODE_NAME}'"
+
+   stage 'Staging: Clean'
+   // Remove the app in the same workspace to avoid reusing packages, node modules etc.
+   // Staging will always create a <branch_name>-inventory-app/ directory instead
+   // of without a prefix so containers run with prefixes of that directory. This helps
+   // container names to not overlap if running multiple staging environments on the same
+   // staging node.
+   sh "sudo rm -rf ${env.BRANCH_NAME}-inventory-app/"
+
+   stage 'Staging: Git Clone'
+   // Clone the inventory app with the Github Bot user.
+   sh "git clone -b ${env.BRANCH_NAME} https://${env.GITUSER}:${env.GITTOKEN}@github.com/ClusterHQ/inventory-app ${env.BRANCH_NAME}-inventory-app/"
+
+   stage 'Staging: Run staging environment'
+   String staging_vs;
+   String staging_snap;
+   if (env.BRANCH_NAME == "master"){
+      // **********************************************
+      //         Do not change unless proposing
+      //         that master use a new snapshot
+      // **********************************************
+      // Volumeset the snapshot belongs to for master
+      staging_vs = 'inventory-app'
+      // Snapshot used for tests in master
+      staging_snap = 'initial_ia_snap'
+      echo "Using Snapshot ${staging_snap} for branch: master"
+   }else{
+      // **********************************************
+      //  Set 'vs' and 'snap' below to use a different 
+      //     branch for your build and tests in CI.
+      // **********************************************
+      // Volumeset the snapshot belongs to for dev branch
+      staging_vs = 'inventory-app'
+      // Snapshot used for tests in master
+      staging_snap = 'initial_ia_snap'
+      echo "Using Snapshot: ${staging_snap} Branch: ${env.BRANCH_NAME}"
+   }
+
+   // Flocker Hub endpoint.
+   def staging_ep = "http://ec2-54-166-4-3.compute-1.amazonaws.com:8084"
+
+   // Run staging
+   sh "sudo ${env.BRANCH_NAME}-inventory-app/ci-utils/runstaging.sh ${staging_vs} ${staging_ep} ${staging_snap} ${env.BRANCH_NAME} ${env.BUILD_URL} ${env.BUILD_NUMBER}"
+}
---
+++ ci-utils/runstaging.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Script to run staging
+# Needs to be run as SUDO
+
+# -------------------- Params ---------------------------------------
+# VOLUMESET        is a Flocker Hub Volumeset, which owns snapshots and variants
+# HUBENDPOINT      is the Flocker Hub URL endpoint used by the CLI.
+# SNAP             is a Flocker Hub Snapshot
+# GITBRANCH        is the Github Branch name being built, it is provided by the Jenkins env.
+# JENKINSBUILDURL  is the Jenkins Build ID URL, it is provided by the Jenkins env.
+# JENKINSBUILDN    is the Jenkins Build Number
+# --------------------- END -----------------------------------------
+
+VOLUMESET=$1
+HUBENDPOINT=$2
+SNAP=$3
+GITBRANCH=$4
+JENKINSBUILDURL=$5
+JENKINSBUILDN=$6
+ENV="staging"
+
+check_for_failure() {
+   if [ $? -eq 0 ]; then
+      echo "OK"
+   else
+      echo "FAIL"
+      exit 1
+   fi
+}
+
+init_fli(){
+   # Check if init has been run or if its a new slave.
+   if [ ! -f /tmp/fliinitdone ]; then
+      /opt/clusterhq/bin/dpcli init --zpool chq -f || true
+      touch /tmp/fliinitdone
+      check_for_failure
+      # vhut token is set as a secret inside the jenkins master
+      /opt/clusterhq/bin/dpcli set tokenfile /root/vhut.txt
+      check_for_failure
+      /opt/clusterhq/bin/dpcli set volumehub $HUBENDPOINT
+      check_for_failure
+   fi
+}
+
+use_snapshot() {
+   echo "Use a specific snapshot"
+   # Run `use_snap.sh` which pulls and creates volume from snapshot.
+   # this script with modify in place the docker-compose.yml file
+   # and add the /chq/<UUID> volume.
+   ${GITBRANCH}-inventory-app/ci-utils/use_snap.sh ${VOLUMESET} ${SNAP} ${GITBRANCH} ${ENV} ${JENKINSBUILDN}
+}
+
+start_app() {
+   echo "Start with snapshot"
+   # Start the application with the volume-from-snapshot.
+   # Output so we can debug whether snapshot was placed
+   # and start the compose app.
+   cat ${GITBRANCH}-inventory-app/docker-compose.yml
+   /usr/local/bin/docker-compose -f ${GITBRANCH}-inventory-app/docker-compose.yml up -d --build --remove-orphans
+   # Show the containers in the log so we know what port to access.
+   # we could use more accurate filtering, see https://docs.docker.com/engine/reference/commandline/ps/
+   docker ps --last 2
+   check_for_failure
+}
+
+# Used in teardown() and publish_staging_env()
+L_NO_DASH_GITBRANCH=$(echo ${GITBRANCH//-} | tr '[:upper:]' '[:lower:]')
+
+teardown() {
+   echo "Teardown app if running"
+   # Tear down the application and database again.
+   /usr/local/bin/docker-compose -f ${GITBRANCH}-inventory-app/docker-compose.yml stop
+   /usr/local/bin/docker-compose -f ${GITBRANCH}-inventory-app/docker-compose.yml rm -f
+   docker volume rm ${L_NO_DASH_GITBRANCH}inventoryapp_rethink-data || true
+}
+
+publish_staging_env() {
+   host=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+   frontend_port=$(cut -d ":" -f 2 <<< $(sudo docker port ${L_NO_DASH_GITBRANCH}inventoryapp_frontend_1))
+   check_for_failure
+   rethinkdb_port=$(cut -d ":" -f 2 <<< $(sudo docker port ${L_NO_DASH_GITBRANCH}inventoryapp_db_1 | grep 28015))
+   check_for_failure
+   rethinkdb_ui_port=$(cut -d ":" -f 2 <<< $(sudo docker port ${L_NO_DASH_GITBRANCH}inventoryapp_db_1 | grep 8080))
+   check_for_failure
+   echo "Your staging environment is available at ${host}:${frontend_port}"
+   echo "Your staging database is available at ${host}:${rethinkdb_port}"
+   echo "Your staging database UI is available at ${host}:${rethinkdb_ui_port}"
+}
+
+
+run_group() {
+   echo "Bringing up staging for ${GITBRANCH}-inventory-app with snapshot: ${SNAP}"
+   init_fli
+   teardown
+   use_snapshot
+   start_app
+   publish_staging_env
+}
+
+run_group
+
