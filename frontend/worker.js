b18e7d41522ee1bc464d3f7e5e5c6b58d79053f4
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-10-13 16:35:02 -0400

    Indexed order_by can only be performed on a TABLE or TABLE_SLICE

--- frontend/worker.js
+++ frontend/worker.js
@@ -72,7 +72,7 @@ module.exports.run = function (worker) {
               // Because we declared the dealer field above, it is available in here.
               // This allows us to tranform/filter the Product collection based on a specific dealer
               // ID provided by the frontend.
-              return fullTableQuery.filter(r.row('dealership').eq(vehicleFields.dealership)).orderBy({index: 'id'})
+              return fullTableQuery.orderBy({index: 'id'}).filter(r.row('dealership').eq(vehicleFields.dealership))
             }
           }
         },
